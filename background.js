26adcf5f9ea3d5e527a38620df48ab9fafc3a481
Martin Giger <martin@humanoids.be>
2021-02-06 04:06:29 +0100

    chore: license header

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+// Copyright (c) 2021 Martin Giger
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 async function checkTabURL(tab) {
     if(tab.id && tab.url && /^https?:\/\/matrix.to\/#\//.test(tab.url)) {
         const [
